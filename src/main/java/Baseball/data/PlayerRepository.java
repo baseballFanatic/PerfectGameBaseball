76cfb9500972e6494ff12d81768bc2fc5be72b56
Clint Riggan <clint.riggan@abcfinancial.com>
2017-08-18 16:23:52 -0500

    Added in configuration and coding for Spring Boot.
    Signed-off-by: Clint Riggan <clint.riggan@abcfinancial.com>

---
+++ src/main/java/Baseball/data/PlayerRepository.java
@@ -0,0 +1,36 @@
+package Baseball.data;
+
+import Baseball.Batter;
+import Baseball.Database;
+import Baseball.Player;
+import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class PlayerRepository {
+    private static final List<Player> ALL_PLAYERS = Arrays.asList(
+        new Player("Clint", "Riggan", "criggan01"),
+        new Player("James", "Sutherland", "jsutherl01"),
+        new Player("Luke", "Sutherland", "lsutherl01"),
+        new Player("Matt", "Hepburn", "mhepburn01")
+    );
+
+    public PlayerRepository() throws ClassNotFoundException, SQLException, InstantiationException {
+    }
+
+    public Player findByName(String name) {
+        for(Player player : ALL_PLAYERS) {
+            if(player.getNameFirst().equals(name)) {
+                return player;
+            }
+        }
+        return null;
+    }
+
+    public List<Player> getAllPlayers() {
+        return ALL_PLAYERS;
+    }
+}
